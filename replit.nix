5324e24035957b10d2abced4548355033f3939e8
morelo <morelo.lolo@gmail.com>
2023-09-27 18:23:46 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{ pkgs }: {
+	deps = [
+		pkgs.python38
+	];
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([
+      # Neded for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+			pkgs.SDL2
+    ] ++ (with pkgs.xlibs; [ libX11 libXext libXinerama libXcursor libXrandr libXi libXxf86vm ]));
+
+    PYTHONBIN = "${pkgs.python38}/bin/python3.8";
+  };
+}
